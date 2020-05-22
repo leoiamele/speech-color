a3fabc86a9237357da684ff6a3c43f4ee605d277
Leandro Iamele <leandroiamele@gmail.com>
2020-05-22 09:59:52 -0300

    Add violet, orange and update error page

--- app.js
+++ app.js
@@ -18,10 +18,17 @@ function runSpeech(){
   recognition.onresult = function(event) {
     var last = event.results.length - 1;
     var command = event.results[last][0].transcript;
-
-    switch (command) {
+    console.log(command.toLowerCase())
+    
+    switch (command.toLowerCase()) {
       case 'rojo':
         document.body.style.backgroundColor = "red";
+        break;
+      case 'violetta':
+        document.body.style.backgroundColor = "mediumorchid";
+        break;
+      case 'naranja':
+        document.body.style.backgroundColor = "orange";
         break;
       case 'azul':
         document.body.style.backgroundColor = "blue";
@@ -32,11 +39,12 @@ function runSpeech(){
       case 'amarillo':
         document.body.style.backgroundColor = "yellow";
         break;
-      case 'Celeste' || 'celeste':
+      case 'celeste':
         document.body.style.backgroundColor = "lightblue";;
         break;
       default:
-        document.body.style.backgroundColor = "black";
+        document.body.style.backgroundColor = "grey";
+        showError(`Ups... Lo siento pero no se que color es ${command}`)
         console.log(`Lo siento pero no se que color es ${command}`);
     }
 };
@@ -47,3 +55,17 @@ function runSpeech(){
 
 }
 
+function showError(error) {
+  const errorDiv = document.createElement('div');
+  const alertDiv = document.getElementById('alert');
+
+  errorDiv.className = 'alert alert-dark mt-5'
+  errorDiv.innerText = error;
+  alertDiv.appendChild(errorDiv)
+
+  //setTimeout(clearError, 2500)
+
+  function clearError() {
+    alertDiv.removeChild(alertDiv.firstChild);
+  }
+}
